a600d790a36c7f5d658b9439fa644a41c5192a69
Cesar Brandon <cesarbrandon000@gmail.com>
2022-05-08 01:22:38 -0500

    adding transpiler

---
+++ public/index.js
@@ -0,0 +1,9 @@
+const dark = document.querySelector('#moon');
+const body = document.querySelector('body');
+const moon = document.querySelector('.moon');
+
+dark.addEventListener('click', e => {
+		body.classList.toggle('dark');
+})
+
+
